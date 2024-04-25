fcc8e4b6421bd23a113739f5c7746f8ea3d8c4b9
AbdulRahman-Sharief <abdulrahmansharief2@gmail.com>
2024-04-25 03:50:14 +0200

    cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    crossOrigin:['https://filtering-system-one.vercel.app/'],
+    images:['https://filtering-system-one.vercel.app/']
+};
 
 export default nextConfig;
