5baff71ca22dde06fb41241cd47acc222bdb1c74
AbdulRahman-Sharief <abdulrahmansharief2@gmail.com>
2024-04-25 03:53:54 +0200

    bug fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,14 @@
 const nextConfig = {
     crossOrigin:['https://filtering-system-one.vercel.app/'],
     // images:['https://filtering-system-one.vercel.app/']
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "**",
-          },
-        ],
-      },
+    // images: {
+    //     remotePatterns: [
+    //       {
+    //         protocol: "https",
+    //         hostname: "**",
+    //       },
+    //     ],
+    //   },
 };
 
 export default nextConfig;
